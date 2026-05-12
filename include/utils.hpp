871e8e158c9e1deacc961119f9ba118870df962d
kyprus <kyprus@kyprusui-Macmini.local>
2026-05-12 12:56:21 +0900

    trajectory length increased

--- include/utils.hpp
+++ include/utils.hpp
@@ -162,9 +162,9 @@ static inline void circle_pva(double t_sec, Eigen::Vector3d& p_d, Eigen::Vector3
 }
 
 static inline void l_traj_pva(double t_sec, Eigen::Vector3d& p_d, Eigen::Vector3d& v_d, Eigen::Vector3d& a_d) {
-  constexpr double lx_ = 2.0;              // width in X [m]
+  constexpr double lx_ = 2.5;              // width in X [m]
   constexpr double ly_ = 0.0;              // width in Y [m]
-  constexpr double T_  = 2.5;              // base period [sec]
+  constexpr double T_  = 3.0;              // base period [sec]
   constexpr double f   = 2.0 * M_PI / T_;  // [rad/s]
 
   double tau = std::fmod(t_sec, 6.0 * T_);
